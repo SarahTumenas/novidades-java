7745d578ed96bb3948e1e88ac8bbfd63a5a40508
Sarah Tumenas <sarah.marques@spread.com.br>
2022-08-10 16:38:33 -0300

    factory method - java 9

--- src/main/java/br/com/alura/factorymethod/Teste.java
+++ src/main/java/br/com/alura/factorymethod/Teste.java
@@ -1,17 +1,26 @@
 package br.com.alura.factorymethod;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Teste {
 
+	//Novidade Java 9 -Factory Method para Collection
 	public static void main(String[] args) {
+		List<String> nomes = List.of("primeiroNome", "segundoNome", "terceiroNome");
+		System.out.println(nomes);
+		//nomes.forEach(nome -> System.out.println(nome));
+		Set.of("quartoNome");
+		Map.of("nome", "José");
+
+
+
+		/*
 		ArrayList<String> nomes = new ArrayList<>();
 		nomes.add("primeiroNome");
 		nomes.add("segundoNome");
 		nomes.add("terceiroNome");
 		List<String> nomesImutavel = Collections.unmodifiableList(nomes);
 		System.out.println(nomesImutavel);
+		*/
 	}
 }
