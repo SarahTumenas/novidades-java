38956d200eaa71436ec2ce27f5261bfe00f87b12
Sarah Tumenas <sarah.marques@spread.com.br>
2022-08-10 16:48:25 -0300

    inferência de variáveis - java 10

--- src/main/java/br/com/alura/inferenciavariavel/Teste.java
+++ src/main/java/br/com/alura/inferenciavariavel/Teste.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 
 public class Teste {
 
+	//Novidade do Java 10 -Inferência de variável
 	public static void main(String[] args) {
-		HashMap<String, String> cpfPorNomes = new HashMap<String, String>();
+		var cpfPorNomes = new HashMap<String, String>();
 		cpfPorNomes.put("João", "04813189");
 
 		System.out.println(cpfPorNomes);
