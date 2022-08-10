c27d1cd6069579762077432d4b8b14563845241d
Sarah Tumenas <sarah.marques@spread.com.br>
2022-08-10 15:54:08 -0300

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# Novidades do Java: produtividade com novos recursos
+
+## Tópicos abordados:
+
+- Java interativo no JShell
+- Reactive Streams em Java
+- Novidades do Optional
+- Refatorando o código usando inferência de variável
+- Requisições http com a nova API HTTP/2 Client
+- Java Platform Module System (JPMS)
---
+++ src/main/java/br/com/alura/NovidadesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovidadesApplication.class, args);
+	}
+
+}
