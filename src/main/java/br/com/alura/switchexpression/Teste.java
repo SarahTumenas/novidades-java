b1e0246b7d9839a8654cb49565a2e0f49fb32c48
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-01 17:02:24 -0300

    Switch Expressions - java 14

--- src/main/java/br/com/alura/switchexpression/Teste.java
+++ src/main/java/br/com/alura/switchexpression/Teste.java
@@ -2,20 +2,16 @@ package br.com.alura.switchexpression;
 
 public class Teste {
 
+	//Novidade do Java 14 - Switch Expressions
 	public static void main(String[] args) {
 		String nome = "João";
 		switch (nome) {
-		case "Renata": {
-			System.out.println("Acertou: " + nome);
-			break;
-		}
-		case "João": {
-			System.out.println("Acertou: " + nome);
-			break;
-		}
-		default: {
-			System.out.println("Nenhum nome encontrado!!");
-		}
+		case "Renata" -> System.out.println("Acertou: " + nome);
+
+		case "João" -> System.out.println("Acertou: " + nome);
+
+		default -> System.out.println("Nenhum nome encontrado!!");
+
 		}
 	}
 }
